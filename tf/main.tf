92e08adf053067a51faee9b43cf1783363d24576
Chuck Sailer <csailer@acadian-asset.com>
2023-04-21 15:49:03 -0400

    updated deps

--- tf/main.tf
+++ tf/main.tf
@@ -69,7 +69,6 @@ resource "aws_lambda_function" "chux_lambda_parse" {
       AWS_ACCESS_KEY_ID     = var.aws_access_key_id
       AWS_SECRET_ACCESS_KEY = var.aws_secret_access_key
       AWS_REGION            = var.aws_region
-      AWS_BUCKET            = var.aws_bucket
     }
   }
 }
