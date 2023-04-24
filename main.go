323381b2a1b0e1527b8a7bfa66a0ed1505d22d67
Chuck Sailer <csailer@acadian-asset.com>
2023-04-24 14:14:20 -0400

    bullshit

--- main.go
+++ main.go
@@ -1,46 +1,11 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
 	"log"
-	"net/http"
 
 	"github.com/chuxorg/chux-parser/parsing"
 	"github.com/chuxorg/chux-parser/s3"
 )
-
-type ParseLambda struct{}
-
-func (l *ParseLambda) Parse(ctx context.Context, input string) (string, error) {
-
-}
-
-type ParseEvent struct {
-	Input string `json:"input"`
-}
-
-func parseHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Setting up Parse Lambda")
-	pl := ParseLambda{}
-
-	var event ParseEvent
-	err := json.NewDecoder(r.Body).Decode(&event)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	s, err := pl.Parse(r.Context(), event.Input)
-	if err != nil {
-		log.Printf("Error in parseHandler: %v", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{"result": s})
-}
 
 func main() {
 	log.Println("Starting parse application")
